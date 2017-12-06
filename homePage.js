20c76c6ca9db055e223486c57e317cf00a3f5dea
LCLY <lchoo1996@gmail.com>
2017-12-06 01:53:22 -0500

    created navigation bar and modal

---
+++ homePage.js
@@ -0,0 +1,10 @@
+function body_onload(){
+    console.log("it did come in here!");
+    document.getElementById('username').innerHTML = "print u fuck";
+    $('#btnSignup').on('click', function (e) {
+            alert("WTF");
+             //your awesome code here
+        
+    })
+}
+
