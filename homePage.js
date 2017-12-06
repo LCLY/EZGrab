ceead73053d34f285e49203a9c1664be8c2e1abe
LCLY <lchoo1996@gmail.com>
2017-12-06 05:02:07 -0500

    Auto scroll when home button is clicked

--- homePage.js
+++ homePage.js
@@ -1,9 +1,29 @@
-function body_onload(){
-    console.log("it did come in here!");
-    $('#btnSignup').on('click', function (e) {
-            alert("WTF");
-             //your awesome code here
-        
-    })
-}
+//Get
+//var bla = $('#txt_name').val();
+
+//Set
+//$('#txt_name').val(bla);
+
+$(document).ready(function(){
+    $("#backgroundImage").fadeIn(1000);
+    $("#tableDemo").hide();
+
+    //$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+   $("#btnHome").click(function(){
+       console.log("clicked!");    
+       //animation
+        // Handler for .ready() called.
+        $("#tableDemo").show();
+        $('html, body').animate({
+        scrollTop: $('#tableDemo').offset().top
+        }, 'slow');
+   });
+
+   $("#linkToLogin").click(function(){
+        $('#signUpModal').modal('hide');
+   });
+
+   
+
+});
 
