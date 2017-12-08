478188b7f0954dca1606965eb2fc2d15e30232bf
Ryan Teo <ryanteo1996@gmail.com>
2017-12-07 19:45:58 -0500

    added cors to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
 
 app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	return res.send("Hello World");
+});
 
 var port = process.env.PORT || 8000;
 
